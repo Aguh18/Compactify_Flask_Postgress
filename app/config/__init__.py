45d1dcc0453260224c6c0e016f0db30b6a0d62dc
Asep Teguh Hidayat <114033670+Aguh18@users.noreply.github.com>
2024-04-26 11:28:09 +0700

    gatau dahh terserah

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -2,15 +2,16 @@ from flask import Flask
 from .database import db
 from app import app
 from flask_migrate import Migrate
+from dotenv import dotenv_values
+
+
+env_values = dotenv_values(".env")
+databaseUrl = env_values["DATABASE_URL"]
 
 
 
 
-
-
-
-
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/compactify"
+app.config['SQLALCHEMY_DATABASE_URI'] = databaseUrl
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = "app/"
 app.config['SECRET_KEY'] = 'teguh'
