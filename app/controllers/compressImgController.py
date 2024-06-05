870ee17e04db305f6e23950e1a83f7128d94071c
Asep Teguh Hidayat <114033670+Aguh18@users.noreply.github.com>
2024-06-05 19:25:30 +0700

    [feat](compressimage): use dynamic path on  compress image

--- app/controllers/compressImgController.py
+++ app/controllers/compressImgController.py
@@ -7,6 +7,8 @@ from app.config.database import db
 from app.models.fileModel import filesModel
 from PIL import Image 
 import os
+import uuid
+from dotenv import dotenv_values
 
 
 
@@ -24,13 +26,29 @@ def imageCompress():
     elif request.method == "POST":
         if form.validate_on_submit():
             try:
-                print("Ini jalan")
+                env_values = dotenv_values(".env")
+                project_Path = env_values["PATH"]+"app/static/compressImg/"
+                
+               
+                
+                if not os.path.exists(project_Path):
+                    os.makedirs(project_Path)
+                if not os.path.exists(project_Path+"uploads/"):
+                    os.makedirs(project_Path+"uploads/")
+                if not os.path.exists(project_Path+"downloads/"):
+                    os.makedirs(project_Path+"downloads/")
+                
+                uid = str(uuid.uuid4())
+                    
                 file = request.files["file"]
-                file.save("app/static/uploads/" + secure_filename(file.filename) )
-                input_path = "app/static/uploads/" + secure_filename(file.filename)
+                input_path = project_Path+"uploads/" +uid+ secure_filename(file.filename)
+                file.save(input_path )
+                output_Path = project_Path +"downloads/"
+               
                 filename = secure_filename(file.filename)
                 
-                file = compress_img(filename,input_path, new_size_ratio=0.5, quality=50, width=None, height=None, to_jpg=True)
+                file = compress_img(filename,input_path, output_Path,uid, new_size_ratio=0.5, quality=50, width=None, height=None, to_jpg=True)
+                file = "compressImg/downloads/"+file
                 print("nama file adalah", file)
                 
                 db.session.add(filesModel(file))
@@ -61,13 +79,13 @@ def get_size_format(b, factor=1024, suffix="B"):
         b /= factor
     return f"{b:.2f}Y{suffix}"
 
-def compress_img(filename,image_name, new_size_ratio=0.9, quality=50, width=None, height=None, to_jpg=True):
+def compress_img(filename,input_path,output_path,uid, new_size_ratio=0.9, quality=50, width=None, height=None, to_jpg=True):
     # load the image to memory
-    img = Image.open(image_name)
+    img = Image.open(input_path)
     # print the original image shape
     print("[*] Image shape:", img.size)
     # get the original image size in bytes
-    image_size = os.path.getsize(image_name)
+    image_size = os.path.getsize(input_path)
     # print the size before compression/resizing
     print("[*] Size before compression:", get_size_format(image_size))
     if new_size_ratio < 1.0:
@@ -91,13 +109,12 @@ def compress_img(filename,image_name, new_size_ratio=0.9, quality=50, width=None
         new_filename = f"{filename}_compressed{ext}"
     try:
         # save the image with the corresponding quality and optimize set to True
-        img.save( "app/static/CompressImg/" +secure_filename(new_filename)
+        img.save(output_path+secure_filename(new_filename)
                 , quality=quality, optimize=True)
     except OSError:
         # convert the image to RGB mode first
         img = img.convert("RGB")
         # save the image with the corresponding quality and optimize set to True
-        img.save( "app/static/CompressImg/" +secure_filename(new_filename), quality=quality, optimize=True)
+        img.save( output_path+uid+secure_filename(new_filename), quality=quality, optimize=True)
    
-    return secure_filename(new_filename)
-9
+    return uid+secure_filename(new_filename)
